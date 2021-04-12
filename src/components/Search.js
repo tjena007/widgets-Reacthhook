b97c5102ab92d104e376e9777846242450952bd9
Tanmay Jena <tjena2@gmail.com>
2021-04-12 22:15:30 +0530

    implemented wikipedia search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,63 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 const Search = () => {
 
   const[term,setTerm] = useState('');
+  const [results,setResults] = useState([]);
+//   console.log(results);
+
+  useEffect(() => {
+    // console.log("asdghsd")
+    const search = async () =>{
+        const  { data }  = await axios.get('https://en.wikipedia.org/w/api.php',{
+            params: {
+                action : 'query',
+                list : 'search',
+                origin : '*',
+                format: 'json',
+                srsearch: term
+            },
+        });
+        // console.log(data);
+
+        setResults(data.query.search);
+    };
+
+    if(term && !results.length){
+        search();
+    }
+    else{
+        const timeoutID = setTimeout(() => {
+            if(term){
+                search();
+            }
+        },500);
+    
+        return() =>{
+            clearTimeout(timeoutID);
+        }
+    }
+        
+    
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return(
+        <div key={result.pageid} className="item">
+            <div className="right floated content">
+                <a href={`https://en.wikipedia.org?curid=${result.pageid}`} className="ui button">Go</a>
+            </div>
+            <div className="content">
+                <div className="header">
+                    {result.title}
+                </div>
+                <span dangerouslySetInnerHTML={{ __html: result.snippet}}></span>
+            </div>
+        </div>
+    )
+  })
 
   return(
         <div>
@@ -15,6 +70,9 @@ const Search = () => {
                    className="input" 
                    />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     )
